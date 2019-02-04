124f2c47bc48190c85a59a68548079fa25b63dc0
Adam Silverstein <adam@10up.com>
2019-02-04 12:54:43 -0700

    Pause for -block-switcher opening animation.

--- packages/e2e-test-utils/src/transform-block-to.js
+++ packages/e2e-test-utils/src/transform-block-to.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import delay from 'delay';
+
+/**
  * Converts editor's block type.
  *
  * @param {string} name Block name.
@@ -7,5 +12,7 @@ export async function transformBlockTo( name ) {
 	await page.mouse.move( 200, 300, { steps: 10 } );
 	await page.mouse.move( 250, 350, { steps: 10 } );
 	await page.click( '.editor-block-switcher__toggle' );
+	// Pause for -block-switcher opening animation.
+	await delay( 50 );
 	await page.click( `.editor-block-types-list__item[aria-label="${ name }"]` );
 }
