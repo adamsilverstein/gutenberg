7eab49e637c3b6ceaf812527ce19ed69c1e404d5
Adam Silverstein <adam@10up.com>
2019-02-05 10:16:12 -0700

    wait for element vs delay

--- packages/e2e-test-utils/src/transform-block-to.js
+++ packages/e2e-test-utils/src/transform-block-to.js
@@ -1,17 +1,14 @@
 /**
- * External dependencies
- */
-import delay from 'delay';
-
-/**
  * Converts editor's block type.
  *
  * @param {string} name Block name.
  */
 export async function transformBlockTo( name ) {
-	await page.keyboard.press( 'Escape' );
+	await page.mouse.move( 200, 300, { steps: 10 } );
+	await page.mouse.move( 250, 350, { steps: 10 } );
 	await page.click( '.editor-block-switcher__toggle' );
-	// Pause for -block-switcher opening animation.
-	await delay( 50 );
+
+	// Wait for the selector to become available
+	page.waitForSelector( `.editor-block-types-list__item[aria-label="${ name }"]` );
 	await page.click( `.editor-block-types-list__item[aria-label="${ name }"]` );
 }
