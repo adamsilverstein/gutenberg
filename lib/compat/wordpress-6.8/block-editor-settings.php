1045b9a69ca83f5f7617a4211c701d3ad72731e5
Adam Silverstein <adamjs@google.com>
2025-02-03 08:24:52 -0700

    Build typesNotSupportedByServer in loop

--- lib/compat/wordpress-6.8/block-editor-settings.php
+++ lib/compat/wordpress-6.8/block-editor-settings.php
@@ -16,21 +16,13 @@
 function gutenberg_extend_block_editor_settings_with_image_support( $settings ) {
 	$settings['typesNotSupportedByServer'] = array();
 
-	// Check if WebP images can be edited.
-	if ( ! wp_image_editor_supports( array( 'mime_type' => 'image/webp' ) ) ) {
-		$settings['typesNotSupportedByServer']['image/webp'] = true;
+	$image_types_to_check_for_support = array( 'image/webp', 'image/avif', 'image/heic' );
+	foreach ( $image_types_to_check_for_support as $type ) {
+		// Check if image type can be edited
+		if ( ! wp_image_editor_supports( array( 'mime_type' => $type ) ) ) {
+			$settings['typesNotSupportedByServer'][ $type ] = true;
+		}
 	}
-
-	// Check if AVIF images can be edited.
-	if ( ! wp_image_editor_supports( array( 'mime_type' => 'image/avif' ) ) ) {
-		$settings['typesNotSupportedByServer']['image/avif'] = true;
-	}
-
-	// Check if HEIC images can be edited.
-	if ( ! wp_image_editor_supports( array( 'mime_type' => 'image/heic' ) ) ) {
-		$settings['typesNotSupportedByServer']['image/heic'] = true;
-	}
-
 	return $settings;
 }
 add_filter( 'block_editor_settings_all', 'gutenberg_extend_block_editor_settings_with_image_support' );
