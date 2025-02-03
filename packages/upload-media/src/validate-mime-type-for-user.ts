4a761b7922d070669062964cbc454dd0327cdbda
Adam Silverstein <adamjs@google.com>
2025-02-03 09:47:13 -0700

    Revert validateMimeTypeForUser

--- packages/upload-media/src/validate-mime-type-for-user.ts
+++ packages/upload-media/src/validate-mime-type-for-user.ts
@@ -12,14 +12,12 @@ import { getMimeTypesArray } from './get-mime-types-array';
 /**
  * Verifies if the user is allowed to upload this mime type.
  *
- * @param file                     File object.
- * @param wpAllowedMimeTypes       List of allowed mime types and file extensions.
- * @param [serverUnsupportedTypes] List of image types not supported by the server.
+ * @param file               File object.
+ * @param wpAllowedMimeTypes List of allowed mime types and file extensions.
  */
 export function validateMimeTypeForUser(
 	file: File,
-	wpAllowedMimeTypes?: Record< string, string > | null,
-	serverUnsupportedTypes?: string[]
+	wpAllowedMimeTypes?: Record< string, string > | null
 ) {
 	// Allowed types for the current WP_User.
 	const allowedMimeTypesForUser = getMimeTypesArray( wpAllowedMimeTypes );
@@ -39,23 +37,6 @@ export function validateMimeTypeForUser(
 				// translators: %s: file name.
 				__(
 					'%s: Sorry, you are not allowed to upload this file type.'
-				),
-				file.name
-			),
-			file,
-		} );
-	}
-
-	if (
-		serverUnsupportedTypes &&
-		serverUnsupportedTypes.includes( file.type )
-	) {
-		throw new UploadError( {
-			code: 'MIME_TYPE_NOT_SUPPORTED',
-			message: sprintf(
-				// translators: %s: file name.
-				__(
-					'%s: The web server cannot generate responsive image sizes for this image. Convert it to JPEG or PNG before uploading.'
 				),
 				file.name
 			),
