1383705b4da21727e3105926e07830407fbac9e3
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-09-17 12:17:00 -0600

    Try a popover slot

--- packages/editor/src/components/collab-sidebar/index.js
+++ packages/editor/src/components/collab-sidebar/index.js
@@ -58,6 +58,7 @@ function CollabSidebarContent( {
 	setShowCommentBoard,
 	styles,
 	comments,
+	inPopover,
 } ) {
 	const { createNotice } = useDispatch( noticesStore );
 	const { saveEntityRecord, deleteEntityRecord } = useDispatch( coreStore );
@@ -226,6 +227,7 @@ function CollabSidebarContent( {
 				onCommentReopen={ onCommentReopen }
 				showCommentBoard={ showCommentBoard }
 				setShowCommentBoard={ setShowCommentBoard }
+				inPopover={ inPopover }
 			/>
 		</div>
 	);
@@ -400,6 +402,7 @@ export default function CollabSidebar() {
 					styles={ {
 						backgroundColor,
 					} }
+					inPopover
 				/>
 			</PluginSidebar>
 		</>
